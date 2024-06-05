0bc0011b387f662e0e4808114134d879ab585bf4
Han <chinzonghan3101@berkeley.edu>
2024-06-05 23:59:03 +0800

    v0.8 update cors to remove / at the end

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",  # Your frontend origin
     "http://192.168.86.200:3000", # My personal frontend IP
-    "https://apriora-sprint-hans-projects-299e57dd.vercel.app/",
-    "https://apriora-sprint.vercel.app/",
-    "http://20.9.136.70:3000"
+    "https://apriora-sprint-hans-projects-299e57dd.vercel.app",
+    "https://apriora-sprint.vercel.app",
+    "http://20.9.136.70:3000",
 ]
 
 # Add the CORS middleware to your FastAPI application
